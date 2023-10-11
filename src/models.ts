4cc19bde6a88116f901877abf39a03df4f2f3781
Lajos Meszaros <m_lajos@hotmail.com>
2023-10-11 16:51:26 +0200

    refactor(models): simplify rotation expression

--- src/models.ts
+++ src/models.ts
@@ -9,7 +9,7 @@ export const ak47 = await loadOBJ('./ak47/ak47', {
   // scaleUV: new Vector2(1, -1),
   materialFlags: ArxPolygonFlags.None,
   reversedPolygonWinding: true,
-  orientation: new Rotation(MathUtils.degToRad(0), MathUtils.degToRad(0), MathUtils.degToRad(60)),
+  orientation: new Rotation(0, 0, MathUtils.degToRad(60)),
 })
 
 // export const m4a1 = await loadOBJ('./m4a1/m4a1', {
